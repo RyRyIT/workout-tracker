f6de8fd642ad5293e384f1880e5caee0c44a9ff0
Ryan Lackey <74007392+RyRyIT@users.noreply.github.com>
2021-04-08 14:10:54 -0400

    Update htmlRoute.js

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -6,7 +6,7 @@ module.exports = function(app){
   });
 
   app.get("/",function(req,res){    
-      res.sendFile(path.join(__dirname,"../public/index.html"));
+      res.sendFile(path.join(__dirname,"../index.html"));
   });
 
   app.get("/stats",function(req,res){   
